ac01c361dfd8a91b0f139deb81b2358e2222087a
Hatem <hzehani@doing.fr>
2021-01-16 18:55:29 +0100

    Add more checks + clean up

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-// const { dependencies } = require("./package.json");
-// const yarn = require("yarn-api");
 const preparedDeps = [];
 
 let [, , filePath, keepVersions] = process.argv;
@@ -15,6 +13,11 @@ const path = require("path");
 const { sync: spawnSync } = require("cross-spawn");
 const { existsSync, readFileSync } = require("fs");
 
+if (!filePath) {
+  console.error("You have to specify an entry file.");
+  return console.error("USAGE: yarn-global-restore <file> [--keep-versions]");
+}
+
 let fileExists = false;
 
 if (existsSync(path.join(process.cwd(), filePath))) {
@@ -26,19 +29,21 @@ if (existsSync(path.join(filePath))) {
   fileExists = true;
 }
 
-if (fileExists) {
-  let { dependencies } = JSON.parse(readFileSync(filePath));
-  if (dependencies) {
-    for (const key of Object.keys(dependencies)) {
-      preparedDeps.push(`${key}${keepVersions ? "@" + dependencies[key] : ""}`);
-    }
-    const { error } = spawnSync("yarn", ["global", "add", ...preparedDeps], {
-      stdio: "inherit",
-    });
-    if (error) throw error;
-  } else {
-    return console.error("Invalid list file");
-  }
-} else {
+if (!fileExists) {
   return console.error("File does not exist");
 }
+
+let { dependencies } = JSON.parse(readFileSync(filePath));
+
+if (!dependencies) {
+  return console.error("Invalid list file");
+}
+
+for (const key of Object.keys(dependencies)) {
+  preparedDeps.push(`${key}${keepVersions ? "@" + dependencies[key] : ""}`);
+}
+const { error } = spawnSync("yarn", ["global", "add", ...preparedDeps], {
+  stdio: "inherit",
+});
+
+if (error) throw error;
